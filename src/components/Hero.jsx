696e6e284876634370f2d710946a5d94e889a9be
imtiyaj786 <imtiyaj1523@gmail.com>
2023-06-11 19:45:31 +0530

    error fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { Resume } from "./canvas";
+// import { Resume } from "./canvas";
 
 const Hero = () => {
   return (
