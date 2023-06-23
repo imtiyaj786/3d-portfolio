62fffd658be344d81f88e535a0f4bab730bcc3ee
imtiyaj786 <imtiyaj1523@gmail.com>
2023-06-23 13:10:22 +0530

    computer canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,10 +28,10 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <br />
+      {/* <br />
       <br/>
       <br />
-      <ComputersCanvas />
+      <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
