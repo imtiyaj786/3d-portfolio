998cdf7307f9b7231f816266023a1acf0611fc2f
imtiyaj786 <imtiyaj1523@gmail.com>
2024-05-04 17:11:21 +0530

    logo error fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,8 +27,8 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <br />
-      <ComputersCanvas />
+      {/* <br />
+      <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
